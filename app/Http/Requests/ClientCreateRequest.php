1927a562277101bc3eb140eb050841fc8f718a12
Patryk <patryk.zajac0@gmail.com>
2021-02-18 12:42:21 +0100

    [Changes] Rename of request

---
+++ app/Http/Requests/ClientCreateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            "name" => "required|min:3|max:190",
+            "city" => "min:3|max:190",
+            "street" => "min:3|max:190",
+            "street_no" => "min:3|max:20",
+            "country" => "min:3|max:100",
+            "postal_code" => "min:3|max:6"
+        ];
+    }
+}
