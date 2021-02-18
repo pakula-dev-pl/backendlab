f1cda7177836aecee608f71480d57639e7278e80
Patryk <patryk.zajac0@gmail.com>
2021-02-18 10:15:19 +0100

    [Changes] Rename custome update equest

--- app/Http/Requests/CustomerUpdateRequest.php
+++ app/Http/Requests/CustomerUpdateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CustomerRequestUpdate extends FormRequest
+class CustomerUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
